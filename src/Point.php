03cec915e856186940d12f1c7573a390b5a68e13
yf <291323003@qq.com>
2019-06-13 21:29:23 +0800

    up

--- src/Point.php
+++ src/Point.php
@@ -24,13 +24,125 @@ class Point
         $this->startTime = round(microtime(true),4);
     }
 
-    function end($arg = null)
+    function end(?string $status = self::END_SUCCESS,$arg = null)
     {
-        if($this->endStatus){
+        if($this->endStatus != self::END_UNKNOWN){
            return;
         }
-        $this->endStatus = true;
+        $this->endStatus = $status;
         $this->endArg = $arg;
         $this->endTime = round(microtime(true),4);
+    }
+
+    /**
+     * @return float
+     */
+    public function getStartTime(): float
+    {
+        return $this->startTime;
+    }
+
+    /**
+     * @param float $startTime
+     */
+    public function setStartTime(float $startTime): void
+    {
+        $this->startTime = $startTime;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStartArg()
+    {
+        return $this->startArg;
+    }
+
+    /**
+     * @param mixed $startArg
+     */
+    public function setStartArg($startArg): void
+    {
+        $this->startArg = $startArg;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEndTime()
+    {
+        return $this->endTime;
+    }
+
+    /**
+     * @param mixed $endTime
+     */
+    public function setEndTime($endTime): void
+    {
+        $this->endTime = $endTime;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPointName(): string
+    {
+        return $this->pointName;
+    }
+
+    /**
+     * @param string $pointName
+     */
+    public function setPointName(string $pointName): void
+    {
+        $this->pointName = $pointName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTrace()
+    {
+        return $this->trace;
+    }
+
+    /**
+     * @param mixed $trace
+     */
+    public function setTrace($trace): void
+    {
+        $this->trace = $trace;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEndStatus(): string
+    {
+        return $this->endStatus;
+    }
+
+    /**
+     * @param string $endStatus
+     */
+    public function setEndStatus(string $endStatus): void
+    {
+        $this->endStatus = $endStatus;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEndArg()
+    {
+        return $this->endArg;
+    }
+
+    /**
+     * @param mixed $endArg
+     */
+    public function setEndArg($endArg): void
+    {
+        $this->endArg = $endArg;
     }
 }
