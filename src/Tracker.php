1bdf780b4828e245ea6b4f0f9499b9dbd4b32c19
yf <291323003@qq.com>
2019-06-13 20:55:06 +0800

    up

--- src/Tracker.php
+++ src/Tracker.php
@@ -4,10 +4,20 @@
 namespace EasySwoole\Tracker;
 
 
+use EasySwoole\Utility\Random;
+
 class Tracker
 {
     protected $pointList = [];
     protected $attributes = [];
+    protected $trackerId;
+
+    function __construct(?string $trackerId = null)
+    {
+        if(empty($trackerId)){
+            $this->trackerId = Random::character(18);
+        }
+    }
 
     public function addPoint(string $pointName,$arg = null):Point
     {
@@ -30,5 +40,23 @@ class Tracker
     function getPoints():array
     {
         return $this->pointList;
+    }
+
+    function __toString()
+    {
+        $msg = "Tracker Stack:\n";
+        $msg .="\tTrackerId:{$this->trackerId}\n";
+        $msg .= "\tAttributes:\n";
+        foreach ($this->attributes as $key => $attribute){
+            $msg .= "\t\t{$key}:{$attribute}\n";
+        }
+        $msg .= "\tPoints:\n";
+        /** @var Point $point */
+        foreach ($this->pointList as $point){
+            $msg .= "\t\tname:{$point->getPointName()}\n";
+            $msg .= "\t\tstartTime:{$point->getStartTime()}\n";
+            $msg .= "\t\tendTime:{$point->getEndTime()}\n";
+        }
+        return $msg;
     }
 }
